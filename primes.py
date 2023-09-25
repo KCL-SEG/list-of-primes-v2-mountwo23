df7a6ba7cada8df2a28a9d18a22c7a688e67703c
mountwo23 <55633208+mountwo23@users.noreply.github.com>
2023-09-25 16:19:54 +0100

    Assignment completed

--- primes.py
+++ primes.py
@@ -1,6 +1,23 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError('error')
     list = []
+    currentNumber = 2
+    for x in range(number_of_primes):
+        y = x + 1
+        while len(list) < y:
+            if is_prime(currentNumber):
+                list.append(currentNumber)
+            currentNumber = currentNumber + 1
     return list
+
+
+def is_prime(number):
+    for i in range(2, number):
+        if (number % i) == 0:
+            return False
+    return True
